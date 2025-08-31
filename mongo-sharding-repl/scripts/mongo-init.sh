c437f42d82dbdb93752674ca0fa2695d2d3313dd
ArtPro9 <artem.propazhin@gmail.com>
2025-08-31 19:17:52 +0300

    sprint 4

---
+++ mongo-sharding-repl/scripts/mongo-init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker compose exec -T mongos-router mongosh --port 27020 <<EOF
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+use somedb
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+EOF
